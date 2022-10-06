1cffc98e5b997d8337e941a04f4f6886313cd39e
NG <nicholeguan2@gmail.com>
2022-10-06 01:41:17 -0700

    fix current day which did not display correct current date and add timeblocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,36 @@
 var today = moment();
-$("#currentDay").text((today).format("dddd, MMMM Mo"));
+$("#currentDay").text((today).format("dddd, MMMM Do"));
+
+$(document).ready(function() {
+    for(var i = 0; i<9; i++){
+        $( ".container" ).addClass( "time-block row" );
+        $('.container').append(
+            $(document.createElement('p')).attr({
+                class: 'hour col-lg-1 h-100',
+            })
+
+        );
+        $('.hour').each(function(i){
+            if(i+9<13){
+                $(this).text(i+9 +'AM');
+            }
+            else{
+                $(this).text(i+9-12 +'PM');
+            }
+        });
+        $('.container').append(
+            $(document.createElement('input')).attr({
+                type: 'input',
+                class: 'col-lg-10'
+            })
+        );
+        
+        $('.container').append(
+            $(document.createElement('input')).prop({
+                type: 'button',
+                class: 'saveBtn col-lg-1',
+                value: 'Submit'
+            })
+        );
+    }
+});
