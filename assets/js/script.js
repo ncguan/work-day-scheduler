619d555ad65ff24086ec7ae3b40434906a88ea95
NG <nicholeguan2@gmail.com>
2022-10-06 06:20:40 -0700

    add code to allow events to be saved to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,60 @@
 var today = moment();
 $("#currentDay").text((today).format("dddd, MMMM Do"));
 
-$(document).ready(function() {
-    for(var i = 0; i<9; i++){
-        $( ".container" ).addClass( "time-block row" );
-        $('.container').append(
-            $(document.createElement('p')).attr({
-                class: 'hour col-lg-1 h-100',
-            })
 
-        );
-        $('.hour').each(function(i){
-            if(i+9<13){
-                $(this).text(i+9 +'AM');
-            }
-            else{
-                $(this).text(i+9-12 +'PM');
-            }
-        });
-        $('.container').append(
-            $(document.createElement('input')).attr({
-                type: 'input',
-                class: 'col-lg-10'
-            })
-        );
-        
-        $('.container').append(
-            $(document.createElement('input')).prop({
-                type: 'button',
-                class: 'saveBtn col-lg-1',
-                value: 'Submit'
-            })
-        );
-    }
+for(var i = 0; i<9; i++){
+    $( ".container" ).addClass( "time-block row" );
+    $('.container').append(
+        $(document.createElement('p')).attr({
+            class: 'hour col-lg-1 h-100',
+        })
+    );
+    $('.hour').each(function(i){
+        if(i+9<12){
+            $(this).text(i+9 +'AM');
+        }
+        else if (i+9==12){
+            $(this).text(i+9 +'PM');
+        }
+        else{
+            $(this).text(i+9-12 +'PM');
+        }
+    });
+    $('.container').append(
+        $(document.createElement('input')).attr({
+            type: 'input',
+            class: 'eventInput col-lg-10',
+        })
+    );
+    
+    $('.container').append(
+        $(document.createElement('input')).attr({
+            type: 'button',
+            class: 'saveBtn col-lg-1',
+            value: 'Save',
+        })
+    );
+}
+
+var events = new Array(9);
+$('.saveBtn').on('click', function () {
+    var index = $('.saveBtn').index(this);
+    var input = $('.eventInput')
+    var value = input.eq(index).val();
+    events.splice(index,1,value);
+    storeEvents();
 });
+
+renderSchedule();
+function renderSchedule(){
+    var storedEvents = JSON.parse(localStorage.getItem("events"));
+    events = storedEvents
+    for (var i = 0; i < events.length; i++) {
+        var init = events[i];
+        $('.eventInput').eq(i).val(init)
+    }
+}
+
+function storeEvents(){
+    localStorage.setItem("events", JSON.stringify(events));
+}
